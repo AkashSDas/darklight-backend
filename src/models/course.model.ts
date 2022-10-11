09490e392f73e953b94a0db744eb4c9b87fcedd8
AkashSDas <aakashdas368@gmail.com>
2022-10-11 12:04:09 +0530

    create a course model

---
+++ src/models/course.model.ts
@@ -0,0 +1,90 @@
+import { SchemaTypes } from "mongoose";
+
+import { getModelForClass, prop, Severity } from "@typegoose/typegoose";
+
+import { TCourseLessonClass } from "./course-lesson.model";
+import { TImageClass } from "./image.model";
+import { TUserClass } from "./user.model";
+
+// ===============================
+// Enums
+// ===============================
+
+export enum CourseLifecycleStage {
+  DRAFT = "draft",
+  PUBLISHED = "published",
+}
+
+export enum CourseCourseDifficulty {
+  BEGINNER = "beginner",
+  INTERMEDIATE = "intermediate",
+  ADVANCED = "advanced",
+}
+
+// ===============================
+// Models and Sub-documents
+// ===============================
+
+/** Faq Typegoose Class */
+class TFaqClass {
+  @prop({ type: SchemaTypes.String, maxlength: 120, minlength: 3, trim: true })
+  question: string;
+
+  @prop({ type: SchemaTypes.String, maxlength: 120, minlength: 3, trim: true })
+  answer: string;
+}
+
+/** Course Typegoose Class */
+export class TCourseClass {
+  @prop({ type: SchemaTypes.String, maxlength: 120, minlength: 6 })
+  title: string;
+
+  @prop({ type: SchemaTypes.String, maxlength: 120, minlength: 6 })
+  description: string;
+
+  @prop({ type: SchemaTypes.Date, default: Date.now, required: true })
+  lastEditedOn: Date;
+
+  @prop({
+    type: SchemaTypes.String,
+    required: true,
+    enum: CourseLifecycleStage,
+    default: CourseLifecycleStage.DRAFT,
+  })
+  stage: CourseLifecycleStage;
+
+  @prop({ type: SchemaTypes.Array, required: true, default: [] })
+  instructors: TUserClass[];
+
+  @prop({ type: SchemaTypes.Number, min: 0, required: true, default: 0 })
+  price: number;
+
+  @prop({
+    type: SchemaTypes.String,
+    required: true,
+    enum: CourseCourseDifficulty,
+    default: CourseCourseDifficulty.BEGINNER,
+  })
+  difficulty: CourseCourseDifficulty;
+
+  @prop({ type: SchemaTypes.Array, required: true, default: [] })
+  tags: string[];
+
+  @prop({ type: () => TImageClass })
+  coverImage: TImageClass;
+
+  @prop({ type: SchemaTypes.Array, required: true, default: [] })
+  lessons: TCourseLessonClass[];
+
+  @prop({ type: SchemaTypes.Array, required: true, default: [] })
+  faqs: TFaqClass[];
+}
+
+export var CourseModel = getModelForClass(TCourseClass, {
+  schemaOptions: {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    typeKey: "type",
+  },
+  options: { allowMixed: Severity.ALLOW, customName: "course" },
+});
