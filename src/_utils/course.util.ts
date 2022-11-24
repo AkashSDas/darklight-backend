22f9e1844f0900d716797c46bf8d8f68e59dcd6e
AkashSDas <aakashdas368@gmail.com>
2022-11-24 19:05:42 +0530

    add upload img util

--- src/_utils/course.util.ts
+++ src/_utils/course.util.ts
@@ -1,3 +1,9 @@
+import { DocumentType } from "@typegoose/typegoose";
+import cloudinary from "cloudinary";
+import { UploadedFile } from "express-fileupload";
+import { CourseClass } from "../_models/course.model";
+import { COURSE_COVER_IMG_DIR } from "./cloudinary.util";
+
 export enum CourseStage {
   DRAFT = "draft",
   PUBLISHED = "published",
@@ -16,13 +22,26 @@ export enum FileType {
   IMAGE = "image",
 }
 
-export var allowedCourseSettings = [
-  "emoji",
-  "title",
-  "description",
-  "stage",
-  "price",
-  "difficulty",
-  "tags",
-  "faqs",
-];
+// COURSE
+
+export async function updateCourseCoverImage(
+  file: UploadedFile,
+  course: DocumentType<CourseClass>
+) {
+  // Check if the course already as an image, if it's with id
+  // then delete it else upload the new image and update the
+  // course with the new image id and URL
+
+  if (course.coverImage && course.coverImage.id) {
+    // Delete image from cloudinary
+    await cloudinary.v2.uploader.destroy(course.coverImage.id);
+  }
+
+  // Upload the new image in cloudinary
+  var result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+    folder: `${COURSE_COVER_IMG_DIR}/${course._id}`,
+    crop: "scale",
+  });
+
+  return { id: result.public_id, URL: result.secure_url };
+}
