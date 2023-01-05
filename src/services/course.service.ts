b9f76e6b00c0595dac3997783a9f68da0ba00498
AkashSDas <aakashdas368@gmail.com>
2023-01-05 20:10:56 +0530

    add pkg and fix import

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -1,6 +1,7 @@
 import { FilterQuery } from "mongoose";
 
-import { Course, CourseClass } from "../models/course.model";
+import { CourseClass } from "../models/course.model";
+import { Course } from "../models";
 
 export function getCourseService(filter: FilterQuery<CourseClass>) {
   return Course.findOne(filter);
