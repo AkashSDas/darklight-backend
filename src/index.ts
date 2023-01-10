e1a6d301048c850c5073e6b43a0492aaa07191a2
AkashSDas <aakashdas368@gmail.com>
2023-01-10 17:47:40 +0530

    setup mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { config } from "dotenv";
 
 import { app } from "./api";
+import { connectToDB } from "./utils/db";
 import logger from "./utils/logger";
 
 if (process.env.NODE_ENV != "production") config();
+
+connectToDB();
 
 (function startApp() {
   var port = process.env.PORT || 8000;
